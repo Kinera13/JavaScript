e227a9d76d446c085e355b279d06963c27ac4366
Kinera13 <42468070+Kinera13@users.noreply.github.com>
2019-07-09 12:49:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("Hello Mediasoft");
+let m = prompt('первое число', '');
+typeof m
+
+let n = prompt('второе число', '');
+let op = prompt('арифметическая операция', '');
+
+if (op == '+') {
+	alert(m+n);
+}
+
+else if (op == '-'){
+	alert(m-n);
+}
+
+else if (op == '*'){
+	alert(m*n);
+}
+
+else if(op == '/'){
+	alert(m/n);
+}
+
+else {
+	alert('Вычисление невозможно');
+}
